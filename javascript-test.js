dd838a3a54025a076c9ccae4a90d1cfda47bc0c4
Mithun Rathod <mithun.rathod@prisms.in>
2026-03-14 15:54:39 +0530

    js test

--- javascript-test.js
+++ javascript-test.js
@@ -1,19 +1,18 @@
 function printPattern(size) {
-  var middle = Math.floor(size / 2);
-  for (var i = 0; i < size; i++) {
-    var row = i <= middle ? i : size - i - 1;
-    var output = "";
-    for (var j = 0; j < middle - row; j++) {
+  const middle = Math.floor(size / 2);
+  for (let i = 0; i < size; i++) {
+    let row = i <= middle ? i : size - i - 1;
+    let output = "";
+    for (let j = 0; j < middle - row; j++) {
       output += " ";
     }
     output += "*";
     if (row > 0) {
-      for (var k = 0; k < (2 * row - 1); k++) {
+      for (let k = 0; k < (2 * row - 1); k++) {
         output += " ";
       }
       output += "*";
     }
-
     console.log(output);
   }
 }
